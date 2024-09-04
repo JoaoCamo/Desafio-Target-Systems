0178468bb05ccb4eb8bda8377b67164556f2aab0
João Camillo de Moura <joaocamo@gmail.com>
2024-09-04 14:29:36 -0300

    Add files via upload

---
+++ testeTargetSystems/src/main/java/testeTargetSystems/Desafio5.java
@@ -0,0 +1,39 @@
+package testeTargetSystems;
+
+import java.util.Scanner;
+
+public class Desafio5
+{
+	public static void main(String[] args)
+	{
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a string para inverter: ");
+        String input = scanner.nextLine();
+
+        scanner.close();
+
+        String reversed = inverterString(input);
+
+        System.out.println("String invertida: " + reversed);
+    }
+
+    // Função que inverte a string
+    public static String inverterString(String str)
+    {
+        char[] caracteres = str.toCharArray();
+        int esquerda = 0;
+        int direita = caracteres.length - 1;
+
+        while (esquerda < direita)
+        {
+            char temp = caracteres[esquerda];
+            caracteres[esquerda] = caracteres[direita];
+            caracteres[direita] = temp;
+            esquerda++;
+            direita--;
+        }
+
+        return new String(caracteres);
+    }
+}
